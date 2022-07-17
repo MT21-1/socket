3d1d3aa49b704e68c55145da1f7f1f349c06ebd3
mavericktherry <48401892+mavericktherry@users.noreply.github.com>
2022-07-17 21:28:48 +0700

    leave Room

--- index.js
+++ index.js
@@ -17,11 +17,24 @@ const io = new Server(server, {
 
 let rooms = [];
 let host_socket_id = {};
+let socket_host_id = {};
 io.on("connection", (socket) => {
   console.log(`User Connected ${socket.id}`);
-  socket.on("disconnecting", function () {
+  socket.on("disconnect", function () {
     console.log("disconnecting.. ", socket.id);
-    // notifyFriendOfDisconnect(socket);
+    HOST_ID = socket_host_id[socket.id];
+    if (HOST_ID != undefined) {
+      console.log(HOST_ID);
+      let room = rooms.find((r) => r.hostId === HOST_ID);
+      const ROOM_ID = room.roomId;
+      rooms.pop(room);
+      console.log(ROOM_ID + " Successfuly CLOSED ");
+      io.to(ROOM_ID).emit("leave_room", { roomId: ROOM_ID });
+      socket.leave(ROOM_ID);
+    } else {
+      //emit ke host participan dah out.
+      console.log("out");
+    }
   });
   //host rights
   //data = {roomId: "xxx", isStarted: "xxx"}
@@ -29,6 +42,9 @@ io.on("connection", (socket) => {
     const ROOM_ID = data.roomId;
     let room = rooms.find((r) => r.roomId === ROOM_ID);
     host_socket_id[data.hostId] = socket.id;
+    socket_host_id[socket.id] = data.hostId;
+    console.log(host_socket_id);
+    console.log(socket_host_id);
     if (room != undefined) {
       //ini roomID exist, minta generate ulang
       io.to(socket.id).emit("room_exist", true);
@@ -40,6 +56,15 @@ io.on("connection", (socket) => {
     io.to(socket.id).emit("create_room_feedback", data);
   });
 
+  socket.on("close_room", (data) => {
+    console.log(data);
+    const ROOM_ID = data.roomId;
+    let room = rooms.find((r) => r.roomId === ROOM_ID);
+    rooms.pop(room);
+    console.log(ROOM_ID + " Successfuly CLOSED ");
+    io.to(ROOM_ID).emit("leave_room", { roomId: ROOM_ID });
+    socket.leave(ROOM_ID);
+  });
   //participan rights
   socket.on("join_room", (data) => {
     //data = {roomId, userId}
@@ -50,18 +75,30 @@ io.on("connection", (socket) => {
     //ga ada room
     if (room == undefined || room.isStarted == true) {
       console.log(ROOM_ID + " ROOM NOT FOUND");
-      io.to(socket.id).emit("join_room_feedback", false);
+      io.to(socket.id).emit("join_room_feedback", {
+        roomId: ROOM_ID,
+        isSuccess: false,
+      });
       return;
     }
     //join room
     socket.join(data.roomId);
     //SEND SIGNAL TO USER SUCCESS JOIN
-    io.to(socket.id).emit("join_room_feedback", true);
+    io.to(socket.id).emit("join_room_feedback", {
+      roomId: ROOM_ID,
+      isSuccess: true,
+    });
 
     //SEND SIGNAL KE ROOM MASTER dia MASUK
     io.to(host_socket_id[room.hostId]).emit("new_participant_join", {
       participantId: data.participantId,
     });
+  });
+
+  socket.on("leave_room", (data) => {
+    const ROOM_ID = data.roomId;
+    console.log(ROOM_ID + " Successfuly Left ");
+    socket.leave(ROOM_ID);
   });
 });
 
